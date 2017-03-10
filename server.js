2e8568cf2acee388e0a5402ab0fc99d8115c95dd
Alireza Saberi <saberi.ar.eng@gmail.com>
2017-03-09 19:39:31 -0500

    feat: views are set

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
+// Simple server just to make the routing part work
+
 /*
-* @Author: Ali
-* @Date:   2017-03-08 09:09:58
+* @Author: alireza-saberi
+* @Date:   2016-11-02 18:43:25
 * @Last Modified by:   Ali
-* @Last Modified time: 2017-03-08 09:09:58
+* @Last Modified time: 2017-03-09 09:38:08
 */
 
 'use strict';
+/******** Requiring libraries ********/
+var express = require('express');
+var app = express();
+
+
+/******** Setting ********/
+app.disable('x-powered-by');
+app.set('port', 3000 || process.env.PORT);
+
+
+/******** Routing ********/
+app.use(express.static(__dirname + '/dist'));
+
+
+app.listen(app.get('port'), () => {
+    console.log('Server is listening on port ' + app.get('port') + '. Press CTRL-C to terminate.');
+});
